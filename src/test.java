9be70d4cd3b691e3800e8411da80b408fbc190a7
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-11 03:36:57 +0700

    lopPointvaMoveablePoint

--- src/test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        MoveablePoint point = new MoveablePoint(5,7,2,2);
-        System.out.println(point.toString());
-        System.out.println(point.move().toString());
-    }
-}
